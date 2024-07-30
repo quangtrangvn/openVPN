d220c7322fee996ec3b4fa5897c6f6f9c7644293
QUANG TRẠNG <142284250+quangtrangvn@users.noreply.github.com>
2024-07-30 08:59:45 +0700

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -217,8 +217,8 @@ access-control: fd42:42:42:42::/112 allow' >>/etc/unbound/openvpn.conf
 }
 
 function installQuestions() {
-	echo "Welcome to the OpenVPN installer!"
-	echo "The git repository is available at: https://github.com/angristan/openvpn-install"
+	echo "HÍ ANH EM, Welcome to the OpenVPN installer!"
+	echo "The git repository is available at: https://github.com/quangtrangvn/openVPN"
 	echo ""
 
 	echo "I need to ask you a few questions before starting the setup."
